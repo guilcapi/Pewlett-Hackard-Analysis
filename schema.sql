cee1fe99ca5bf67439b49a5e9a9d657bfbe6cc8a
guilcapi <71358370+guilcapi@users.noreply.github.com>
2020-11-08 20:04:13 -0500

    Adding new queries

--- schema.sql
+++ schema.sql
@@ -31,7 +31,29 @@ CREATE TABLE salaries (
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,  
+	dept_no VARCHAR(4) NOT NULL,
+  	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+--FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (dept_no, emp_no)
+);
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE titles(
+	emp_no INT NOT NULL,
+	title varchar not null,
+	from_date date not null,
+	to_date date not null,
+ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+)
+DROP TABLE titles CASCADE;
 
